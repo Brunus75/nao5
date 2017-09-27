cafa9e0b41dbbfc18cdb7a66b316916763d66f88
adilKrichel <32297483+adilKrichel@users.noreply.github.com>
2017-09-27 17:19:16 +0200

    Add files via upload

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,13 +15,16 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+ new FOS\UserBundle\FOSUserBundle(),
+            new NAOVisiteursBundle\NAOVisiteursBundle(),
+            new NAOMembresBundle\NAOMembresBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
